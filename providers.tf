0a0acc5b722bd1647d6e683cf08972352382f3ce
navinyadav <34382893+navinyadav@users.noreply.github.com>
2024-09-01 07:02:16 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
-provider "google"{
-project = "{{terraform-434118}}"
-region  = "us-west1"
-zone    = "us-west1-a"
+provider "google" {
+  project = "{{terraform-434118}}"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
